e42acfd58132d0439a201e88036cfebcb9f0dd7a
Иванов Константин <konstantin.ivanov39@gmail.com>
2017-09-11 20:34:54 +0300

    no message

--- HomeWork_Lesson#2.5/homework_lesson#2_5.py
+++ HomeWork_Lesson#2.5/homework_lesson#2_5.py
@@ -1,19 +1,29 @@
 import subprocess
 import os
 
-source = 'Source'
-result = 'Result'
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-output_dir = os.path.join(current_dir, result)
-input_dir = os.path.join(current_dir, source)
+def convert_file(file_list, current_dir, input_dir, output_dir ):
+    for file in file_list:
+        output_file = os.path.join(output_dir, 'output_' + file)
+        input_file = os.path.join(input_dir, file)
+        convert = os.path.join(current_dir, 'convert')
+        subprocess.run([convert, input_file, '-resize', '200', output_file])
 
-os.mkdir(output_dir) #создаем папку "Result"
 
-file_list = os.listdir(os.path.join(current_dir, source))
+def main():
+    source = 'Source'
+    result = 'Result'
 
-for file in file_list:
-	output_file = output_dir + '\\output_' + file 
-	input_file = input_dir + '\\' + file
-	convert = current_dir + '\\convert' 
-	subprocess.run([convert, input_file, '-resize', '200', output_file])
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    output_dir = os.path.join(current_dir, result)
+    input_dir = os.path.join(current_dir, source)
+
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir) #создаем папку "Result"
+
+    file_list = os.listdir(os.path.join(current_dir, source))
+    convert_file(file_list, current_dir, input_dir, output_dir)
+	
+	
+main()
+
